1758885e565f661317d24469652c8082b9017660
jim-at-jibba <jim@justjibba.net>
2019-04-23 21:46:47 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { data } from "./data";
+/**
+ * understand JSON structure
+ * how to use it in a reusable component
+ * {
+    name: "Cheese sandwich",
+    price: "2.30",
+    description: "A tasty treat"
+  }
+ */
 
-function App() {
+const MenuItem = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='menuItem'>
+      <p>{props.foo.name}</p>
+      <p>{props.foo.description}</p>
+      <p>{props.foo.price}</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Menu</h1>
+      {data.map((item, index) => (
+        <MenuItem key={`${item.id}${index}`} foo={item} />
+      ))}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const data = [
+  {
+    id: "1",
+    name: "Cheese sandwich",
+    price: "2.30",
+    description: "A tasty treat"
+  },
+  {
+    id: "2",
+    name: "Ham sandwich",
+    price: "2.50",
+    description: "A tasty treat"
+  },
+  {
+    id: "3",
+    name: "Tofu sandwich",
+    price: "2.10",
+    description: "A tasty treat"
+  }
+];
+
+export const data2 = [];
+
+export const data3 = [];
+
+export default () => {
+  console.log("DATA", data);
+};
